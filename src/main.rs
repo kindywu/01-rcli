7847a9c425df70ff7839c1539404acf11b13fa21
kindywu <kindywu@outlook.com>
2024-04-21 14:06:47 +0800

    parse cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+use clap::Parser;
+use std::path::Path;
+
+// rcli csv -i input.csv -o output.json --header -d ','
+#[derive(Debug, Parser)]
+#[command(name ="rcli",version,author,about,long_about=None)]
+struct Opts {
+    #[command(subcommand)]
+    cmd: SubCommand,
+}
+
+#[derive(Debug, Parser)]
+enum SubCommand {
+    #[command(name = "csv", about = "Show CSV ,or convert CSV to other formats")]
+    Csv(CsvOpts),
+}
+
+#[derive(Debug, Parser)]
+struct CsvOpts {
+    #[arg(short, long, value_parser = verify_file_is_exist)]
+    input: String,
+    #[arg(short, long, default_value = "output.json")] //"output.json".into()
+    output: String,
+    #[arg(short, long, default_value_t = ',')]
+    delimiter: char,
+    #[arg(long, default_value_t = true)]
+    header: bool,
+}
+
+// &'static 静态->Data段
+fn verify_file_is_exist(file_name: &str) -> Result<String, &'static str> {
+    if Path::new(file_name).exists() {
+        Ok(file_name.into())
+    } else {
+        Err("file does not exist")
+    }
+}
+
+// cargo run -- csv -i assets/juventus.csv
+// select * from read_csv('assets/juventus.csv', auto_detect=true);
 fn main() {
-    println!("Hello, world!");
+    let opts = Opts::parse();
+    println!("{:?}", opts);
 }
