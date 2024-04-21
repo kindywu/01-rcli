d0bc9943520cfeb80b1236efdf0b4bc49a3a4041
kindywu <kindywu@users.noreply.github.com>
2024-04-21 17:32:46 +0800

    Feature/refactoring code (#4)
    * refactoring code
    
    * move the save disk logic outside the processor
    
    * move the save disk logic outside the processor

---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+mod opts;
+mod processor;
+
+pub use opts::{Opts, SubCommand};
+pub use processor::process_csv;
---
+++ src/processor.rs
@@ -0,0 +1,27 @@
+use csv::Reader;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "PascalCase")]
+struct Player {
+    name: String,
+    position: String,
+    #[serde(rename = "DOB")]
+    dob: String,
+    nationality: String,
+    #[serde(rename = "Kit Number")]
+    kit: String,
+}
+
+pub fn process_csv(input: &str) -> Result<String, anyhow::Error> {
+    let mut reader = Reader::from_path(input)?;
+    let mut players = Vec::with_capacity(128);
+    for result in reader.deserialize() {
+        let player: Player = result?;
+        // println!("{:?}", player);
+        players.push(player);
+    }
+    let json = serde_json::to_string_pretty(&players)?;
+    //
+    Ok(json)
+}
