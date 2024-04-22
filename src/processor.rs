3835e6d54d1fb62b1293b41c6f4cf189ab7f4db0
kindywu <kindywu@users.noreply.github.com>
2024-04-22 10:32:35 +0800

    Support dynamic format (#5)

--- src/processor.rs
+++ src/processor.rs
@@ -1,6 +1,7 @@
 use csv::Reader;
 use serde::{Deserialize, Serialize};
 
+#[allow(dead_code)]
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "PascalCase")]
 struct Player {
@@ -15,13 +16,25 @@ struct Player {
 
 pub fn process_csv(input: &str) -> Result<String, anyhow::Error> {
     let mut reader = Reader::from_path(input)?;
-    let mut players = Vec::with_capacity(128);
-    for result in reader.deserialize() {
-        let player: Player = result?;
-        // println!("{:?}", player);
-        players.push(player);
+    let mut records = Vec::with_capacity(128);
+    let header = reader.headers()?.clone();
+    for result in reader.records() {
+        let record = result?;
+        // println!("{:?}", record);
+
+        // let mut map = std::collections::HashMap::new();
+        // for i in 0..header.len() {
+        //     map.insert(header[i].to_string(), serde_json::json!(record[i]));
+        // }
+        // let json_value = serde_json::json!(map);
+        // records.push(json_value);
+
+        let json_value = header
+            .iter()
+            .zip(record.iter())
+            .collect::<serde_json::Value>();
+        records.push(json_value);
     }
-    let json = serde_json::to_string_pretty(&players)?;
-    //
+    let json = serde_json::to_string_pretty(&records)?;
     Ok(json)
 }
